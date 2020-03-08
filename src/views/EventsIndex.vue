00ad98911ed780e9f3f6ef299853fa821550eeb9
Jeneen <jeneen.lemar@gmail.com>
2020-03-08 19:55:45 -0400

    fixes some images on event index for testing

--- src/views/EventsIndex.vue
+++ src/views/EventsIndex.vue
@@ -9,13 +9,16 @@
       <p> Date/Time: {{ event.time_start }}</p>
     </div>
 
-    
-
-
+    <div v-for="category in categories">
+      <p>{{category.name}}</p>
+      <img v-bind:src="category.img_url" height="100" width="100">
+      
+    </div>
   </div>
 </template>
 
 <style>
+
 </style>
 
 <script>
@@ -24,15 +27,20 @@ export default {
   data: function() {
     return {
       message: "Crafting Events List",
-      events: []
+      events: [],
+      categories: []
     };
   },
   created: function() {
     axios.get("/api/events").then(response => {
-      console.log(response.data);
-      this.events = response.data;
+      console.log(response.data); this.events = response.data;
+    });
+    axios.get("/api/categories").then(response => {
+      console.log(response.data); this.categories = response.data;
     });
   },
+
+  
   methods: {}
 };
 </script>
